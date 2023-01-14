adcb79a1e35a4c3e592264ae07a0dbe297250347
AleksTim <my@email.com>
2023-01-14 18:03:35 +0300

    Changes to be committed: 	new file:   pkg/repository/postgres.go 	new file:   schema/000001_init.down.sql 	new file:   schema/000001_init.up.sql  Changes not staged for commit: 	modified:   .gitignore 	modified:   cmd/main.go 	modified:   configs/config.yml 	modified:   go.mod 	modified:   go.sum 	modified:   pkg/repository/repository.go

---
+++ schema/000001_init.down.sql
@@ -0,0 +1,5 @@
+drop table if exists users cascade;
+drop table if exists todo_lists cascade;
+drop table if exists users_lists;
+drop table if exists todo_items cascade;
+drop table if exists lists_items;
