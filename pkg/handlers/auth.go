999081d8f0c43fb9cde2a95ecd1696840be1ab08
aleks-tim <aotimofeev@lis-agro.com>
2023-01-16 15:05:43 +0300

    Добавлен файл Подготовка настройка.txt с описанием установки Migrate

--- pkg/handlers/auth.go
+++ pkg/handlers/auth.go
@@ -5,20 +5,15 @@ import (
 
 	"github.com/aleks-tim/todo-app"
 	"github.com/gin-gonic/gin"
-	// "github.com/sirupsen/logrus"
 )
 
 func (h *Handler) signUp(c *gin.Context) {
 	var input todo.User
-	// logrus.Println("Handler -> signUp()")
 
 	if err := c.BindJSON(&input); err != nil {
 		newErrorResponse(c, http.StatusBadRequest, err.Error())
 		return
 	}
-	// logrus.Printf("	--> Name	: %s", input.Name)
-	// logrus.Printf("	--> Username: %s", input.Username)
-	// logrus.Printf("	--> Password: %s", input.Password)
 
 	id, err := h.services.Autorization.CreateUser(input)
 	if err != nil {
